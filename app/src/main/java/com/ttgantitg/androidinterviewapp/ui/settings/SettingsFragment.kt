d13689ac096309bf1bfea7f525bfd9f2e3d10c51
ttgantitg <ttgantitg@gmail.com>
2020-03-11 17:01:34 +0300

    add dark/light theme

--- app/src/main/java/com/ttgantitg/androidinterviewapp/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/ttgantitg/androidinterviewapp/ui/settings/SettingsFragment.kt
@@ -22,10 +22,6 @@ class SettingsFragment : Fragment() {
         settingsViewModel =
             ViewModelProvider(this).get(SettingsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_settings, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        settingsViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
         return root
     }
 }
