16e4462005d9922dc5980d0f91f728605df0c817
ttgantitg <ttgantitg@gmail.com>
2020-03-11 23:20:50 +0300

    add dark/light theme

--- app/src/main/java/com/ttgantitg/androidinterviewapp/MainActivity.kt
+++ app/src/main/java/com/ttgantitg/androidinterviewapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ttgantitg.androidinterviewapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -12,8 +13,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        checkThemeColor()
         setContentView(R.layout.activity_main)
         createNavGraph()
+    }
+
+    private fun checkThemeColor() {
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.AppDarkTheme)
+        }
     }
 
     private fun createNavGraph() {
--- app/src/main/java/com/ttgantitg/androidinterviewapp/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/ttgantitg/androidinterviewapp/ui/settings/SettingsFragment.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.ttgantitg.androidinterviewapp.R
 
@@ -22,6 +22,24 @@ class SettingsFragment : Fragment() {
         settingsViewModel =
             ViewModelProvider(this).get(SettingsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_settings, container, false)
+        initThemeSwitch(root)
         return root
+    }
+
+    private fun initThemeSwitch(root: View) {
+        val mSwitch = root.findViewById<SwitchCompat>(R.id.sw_theme)
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            mSwitch.isChecked = true
+        }
+        mSwitch.setOnCheckedChangeListener { _, isChecked ->
+            when (isChecked) {
+                true -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                }
+                false -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                }
+            }
+        }
     }
 }
