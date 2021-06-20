6f778cb88c9edde4b3ca84f92bb7610b8649f6c1
ttgantitg <@wsxZAQ1>
2021-06-20 19:50:54 +0300

    fix NightMode bug

--- app/src/main/java/com/ttgantitg/androidinterviewapp/presentation/MainActivity.kt
+++ app/src/main/java/com/ttgantitg/androidinterviewapp/presentation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -19,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         checkThemeColor()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
